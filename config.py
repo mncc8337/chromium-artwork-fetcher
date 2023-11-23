a6f193ed6c10278c5f1eddc8f715d75857f84b48
mncc8337 <mncc8327@gmail.com>
2023-11-23 21:58:24 +0700

    fix not working example #2

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # the location to save the downloaded art
 save_art_location = "~/.config/awesome/artwork.png"
 # the shell command to run when done fetching (optional, leave blank to ignore)
-shell_command = f'awesome-client "awesome.emit_signal(\\"music::set_cover\\", \\"{save_art_location}\\")"'
+shell_command = 'awesome-client "awesome.emit_signal(\'music::set_cover\', awesome_dir..\'artwork.png\')"'
 
 # replace '~' with $HOME
 if '~' in save_art_location:
